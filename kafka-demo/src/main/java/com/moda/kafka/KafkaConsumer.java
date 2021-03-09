fb972e637dfe04f5e90092b02ca36ce3ded273d2
yangzh <819905173@qq.com>
2021-03-09 16:32:33 +0800

    added(kafka消费者)

---
+++ kafka-demo/src/main/java/com/moda/kafka/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.moda.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class KafkaConsumer {
+    @KafkaListener(id = "consumer1",groupId = "felix-group",topicPartitions = {
+            @TopicPartition(topic = "sun", partitions = { "0" }),
+//            @TopicPartition(topic = "topic2", partitions = "0", partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "8"))
+    })
+    public void onMessage2(ConsumerRecord<?, ?> record) {
+        System.out.println("topic:"+record.topic()+"|partition:"+record.partition()+"|offset:"+record.offset()+"|value:"+record.value());
+    }
+}
